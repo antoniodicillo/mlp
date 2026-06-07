6852f5be2f76b3fd66c25bcf392d6154ed3be97a
antoniodicillo <antoniofilisettidicillo@gmail.com>
2026-06-07 17:19:28 -0300

    feat: foward pass e backpropagation

--- mlp/network.py
+++ mlp/network.py
@@ -8,11 +8,49 @@ class MLP:
         self.biases = []
 
         for i in range(len(layer_sizes) - 1):
-            entrada = layer_sizes[i]
-            saida   = layer_sizes[i + 1]
+            input = layer_sizes[i]
+            output   = layer_sizes[i + 1]
 
-            W = np.random.randn(entrada, saida) * 0.01
-            b = np.zeros((1, saida))
+            W = np.random.randn(input, output) * 0.01
+            b = np.zeros((1, output))  
 
             self.weights.append(W)
-            self.biases.append(b)
+            self.biases.append(b) 
+    def forward(self, X):
+        self.Zs = []
+        self.As = [X]
+
+        A = X
+        for i, (W, b) in enumerate(zip(self.weights, self.biases)):
+            Z = A @ W + b
+            self.Zs.append(Z)
+
+            last_layer = (i == len(self.weights) - 1)
+            if(last_layer):
+                A = softmax(Z)
+            else:
+                A = relu(Z)
+
+            self.As.append(A)
+
+        return A
+    def backward(self, y_true):
+        self.dWs = []
+        self.dbs = []
+        batch_size = y_true.shape[0]
+
+        dZ = self.As[-1] - y_true
+        for i in reversed(range(len(self.weights))):
+            A_old = self.As[i]
+
+            dW = A_old.T @ dZ / batch_size
+            db = np.sum(dZ, axis=0, keepdims=True) / batch_size
+
+            self.dWs.insert(0, dW)
+            self.dbs.insert(0, db)
+
+            if i > 0:
+                dA = dZ @ self.weights[i].T
+                dZ = dA * relu_derivative(self.Zs[i - 1])
+ 
+
